d61f76be9b800f474481b53e9db49c942b4cec1f
Daniel Toma <danieltoma@ubalumni.com>
2019-01-12 19:41:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(total=0, discount=(20))
+    @total = total
+    @discount = discount
+  end
+
+
+
+  def add_item(title, price, quantity=(1))
+
+    @total += price * quantity * self.apply_discount
+    
+  end
+
+  def apply_discount
+    return (1 - (@discount/100))
+  end
+end

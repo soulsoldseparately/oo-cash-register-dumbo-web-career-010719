38d8ed38bb5ce8cf1085e5bd180ee8721a00b172
Daniel Toma <danieltoma@ubalumni.com>
2019-01-12 19:52:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   def add_item(title, price, quantity=(1))
     @subtotal = price * quantity
     @discounted_subtotal = self.apply_discount
-    @total += @discounted_subtotal #self.apply_discount
+    @total = @discounted_subtotal #self.apply_discount
     #self.apply_discount
   end
 

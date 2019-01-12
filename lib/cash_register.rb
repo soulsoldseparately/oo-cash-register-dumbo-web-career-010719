3ca708cf4671b3d4f81f04e7d82c042c0f2fa96b
Daniel Toma <danieltoma@ubalumni.com>
2019-01-12 22:22:35 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,21 +2,11 @@ class CashRegister
 
   attr_accessor :total, :discount, :last_transaction_amount, :items
 
-  def initialize(discount = 0)
+  def initialize(total, discount = 0)
     @total = 0
     @discount = discount
     @items = []
   end
-
-  # attr_accessor :total
-  # attr_reader :discount
-  #
-  # #def initialize(total=0, discount=(20))
-  # def initialize(total=0, discount=20)
-  #   @total = total
-  #   @discount = discount
-  # end
-
 
   def add_item(title,price,quantity = 1)
     if quantity>1
@@ -33,18 +23,6 @@ class CashRegister
     @total
   end
 
-  # def add_item(title, price, quantity=1)
-  #   puts "discount = #{@discount}%"
-  #   puts "total-pt1 = #{@total}"
-  #
-  #
-  #   @total += (price * quantity) * self.apply_discount
-  #
-  #   puts "total = #{@total}"
-  #   puts "price = #{price}, quantity = #{quantity}"
-  #
-  # end
-
   def apply_discount()
     if @discount > 0
       @discount = @discount/100.to_f
@@ -58,9 +36,4 @@ class CashRegister
   def void_last_transaction()
     @total -= @last_transaction_amount
   end
-
-  # def apply_discount
-  #
-  #   1 - @discount/100.0
-  # end
 end
